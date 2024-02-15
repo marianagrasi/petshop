1154e2c84e0b22e2133b4b8a9f105584c0d629e3
Jesus <ja2291774@gmail.com>
2024-02-14 21:13:54 -0500

    Se hizo la creacion de un modal dinamico para ayudar a no sobrecargar la pagina

---
+++ controllers/modal.js
@@ -0,0 +1,51 @@
+// Función para abrir el modal con información dinámica
+function abrirModal1(producto) {
+    var modal = document.getElementById("dynamicProductModal");
+
+    // Configura los elementos del modal con la información del producto
+    document.getElementById("modalProductImage").src = producto.imagen;
+    document.getElementById("modalProductTitle").innerText = producto.nombre;
+    document.getElementById("modalProductPrice").innerText = producto.precio;
+    document.getElementById("modalProductDescription").innerText = producto.descripcion;
+    modal.style.display = "block";
+}
+
+// Asigna el evento de clic a los botones de Comprar
+document.getElementById("btnComprar1").addEventListener("click", function() {
+    // Configura la información del primer producto
+    abrirModal1({
+        imagen: "../assets/img/Catalogo/pañueltagato1.jpg",
+        nombre: "Pañueleta",
+        precio: "$30.000",
+        descripcion: "Descripción del producto 1"
+    });
+});
+
+document.getElementById("btnComprar2").addEventListener("click", function() {
+    // Configura la información del segundo producto
+    abrirModal1({
+        imagen: "../assets/img/Catalogo/pañueletagato2.jpg",
+        nombre: "Pañueleta",
+        precio: "$27.500",
+        descripcion: "Descripción del producto 2"
+    });
+});
+
+document.getElementById("btnComprar3").addEventListener("click", function() {
+    // Configura la información del tecer producto
+    abrirModal1({
+        imagen: "../assets/img/Catalogo/pañueltagato3.jpg",
+        nombre: "Pañueleta",
+        precio: "$29.990",
+        descripcion: "Descripción del producto 3"
+    });
+});
+
+//puedes seguir añadiendo más copiando y pegando el mismo codigo y cambiandole los id
+
+
+//funcion de cerrar modal 
+function cerrarModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
