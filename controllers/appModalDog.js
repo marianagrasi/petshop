0a2c9e0d0d5882f2f0b12662a790a16490b3530d
Estefap12 <estefa991235@gmail.com>
2024-02-19 19:38:57 -0500

    Se realiza seccion de perros con sus respectivos modales y catalogos

---
+++ controllers/appModalDog.js
@@ -0,0 +1,287 @@
+/*Collar1 */
+function img12(anything) {
+    document.querySelector('.slide12').src = anything;
+  }
+
+   function changes(change) {
+    const line = document.querySelector('.home');
+    line.style.background = change;
+  }
+
+   let openModal12 = document.querySelector('.dog12');
+   let modal12 = document.querySelector('.modal12');
+   let closeModal12 = document.querySelector('.modal_close12');
+   
+   openModal12.addEventListener('click', (a)=>{
+       a.preventDefault();
+       modal12.classList.add('modal--show12');
+   });
+   
+   closeModal12.addEventListener('click', (a)=>{
+       a.preventDefault();
+       modal12.classList.remove('modal--show12');
+   });
+ 
+ /*Collar2 */
+function img13(anything) {
+    document.querySelector('.slide13').src = anything;
+  }
+
+  function change(change) {
+    const line = document.querySelector('.home');
+    line.style.background = change;
+  }
+ 
+  let openModal13 = document.querySelector('.dog13');
+  let modal13 = document.querySelector('.modal13');
+  let closeModal13= document.querySelector('.modal_close13');
+  
+  openModal13.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal13.classList.add('modal--show13');
+  });
+  
+  closeModal13.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal13.classList.remove('modal--show13');
+  });
+
+ /*Collar3 */
+ function img14(anything) {
+    document.querySelector('.slide14').src = anything;
+  }
+
+  function change(change) {
+    const line = document.querySelector('.home');
+    line.style.background = change;
+  }
+ 
+  let openModal14 = document.querySelector('.dog14');
+  let modal14 = document.querySelector('.modal14');
+  let closeModal14= document.querySelector('.modal_close14');
+  
+  openModal14.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal14.classList.add('modal--show14');
+  });
+  
+  closeModal14.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal14.classList.remove('modal--show14');
+  });
+
+  /*Pañuelo 1 */
+ function img15(anything) {
+    document.querySelector('.slide15').src = anything;
+  }
+
+  function change(change) {
+    const line = document.querySelector('.home');
+    line.style.background = change;
+  }
+ 
+  let openModal15 = document.querySelector('.dog15');
+  let modal15 = document.querySelector('.modal15');
+  let closeModal15= document.querySelector('.modal_close15');
+  
+  openModal15.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal15.classList.add('modal--show15');
+  });
+  
+  closeModal15.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal15.classList.remove('modal--show15');
+  });
+  /*Pañuelo 2 */
+ function img16(anything) {
+    document.querySelector('.slide16').src = anything;
+  }
+
+  function change(change) {
+    const line = document.querySelector('.home');
+    line.style.background = change;
+  }
+ 
+  let openModal16 = document.querySelector('.dog16');
+  let modal16 = document.querySelector('.modal16');
+  let closeModal16= document.querySelector('.modal_close16');
+  
+  openModal16.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal16.classList.add('modal--show16');
+  });
+  
+  closeModal16.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal16.classList.remove('modal--show16');
+  });
+
+   /*Pañuelo 3 */
+ function img17(anything) {
+    document.querySelector('.slide17').src = anything;
+  }
+
+  function change(change) {
+    const line = document.querySelector('.home');
+    line.style.background = change;
+  }
+ 
+  let openModal17 = document.querySelector('.dog17');
+  let modal17 = document.querySelector('.modal17');
+  let closeModal17= document.querySelector('.modal_close17');
+  
+  openModal17.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal17.classList.add('modal--show17');
+  });
+  
+  closeModal17.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal17.classList.remove('modal--show17');
+  });
+
+    /*Arnes 1 */
+ function img18(anything) {
+    document.querySelector('.slide18').src = anything;
+  }
+
+  function change(change) {
+    const line = document.querySelector('.home');
+    line.style.background = change;
+  }
+ 
+  let openModal18 = document.querySelector('.dog18');
+  let modal18 = document.querySelector('.modal18');
+  let closeModal18= document.querySelector('.modal_close18');
+  
+  openModal18.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal18.classList.add('modal--show18');
+  });
+  
+  closeModal18.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal18.classList.remove('modal--show18');
+  });
+
+
+   /*Arnes 2*/
+   function img19(anything) {
+    document.querySelector('.slide19').src = anything;
+  }
+
+  function change(change) {
+    const line = document.querySelector('.home');
+    line.style.background = change;
+  }
+ 
+  let openModal19 = document.querySelector('.dog19');
+  let modal19 = document.querySelector('.modal19');
+  let closeModal19= document.querySelector('.modal_close19');
+  
+  openModal19.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal19.classList.add('modal--show19');
+  });
+  
+  closeModal19.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal19.classList.remove('modal--show19');
+  });
+
+  /*Arnes 3*/
+  function img20(anything) {
+    document.querySelector('.slide20').src = anything;
+  }
+
+  function change(change) {
+    const line = document.querySelector('.home');
+    line.style.background = change;
+  }
+ 
+  let openModal20 = document.querySelector('.dog20');
+  let modal20 = document.querySelector('.modal20');
+  let closeModal20= document.querySelector('.modal_close20');
+  
+  openModal20.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal20.classList.add('modal--show20');
+  });
+  
+  closeModal20.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal20.classList.remove('modal--show20');
+  });
+  /*Sueter 1*/
+  function img21(anything) {
+    document.querySelector('.slide21').src = anything;
+  }
+
+  function change(change) {
+    const line = document.querySelector('.home');
+    line.style.background = change;
+  }
+ 
+  let openModal21 = document.querySelector('.dog21');
+  let modal21 = document.querySelector('.modal21');
+  let closeModal21= document.querySelector('.modal_close21');
+  
+  openModal21.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal21.classList.add('modal--show21');
+  });
+  
+  closeModal21.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal21.classList.remove('modal--show21');
+  });
+
+
+  /*Sueter 2*/
+  function img22(anything) {
+    document.querySelector('.slide22').src = anything;
+  }
+
+  function change(change) {
+    const line = document.querySelector('.home');
+    line.style.background = change;
+  }
+ 
+  let openModal22 = document.querySelector('.dog22');
+  let modal22 = document.querySelector('.modal22');
+  let closeModal22= document.querySelector('.modal_close22');
+  
+  openModal22.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal22.classList.add('modal--show22');
+  });
+  
+  closeModal22.addEventListener('click', (e)=>{
+      e.preventDefault();
+      modal22.classList.remove('modal--show22');
+  });
+
+    /*Sueter 3*/
+    function img23(anything) {
+        document.querySelector('.slide23').src = anything;
+      }
+    
+      function change(change) {
+        const line = document.querySelector('.home');
+        line.style.background = change;
+      }
+     
+      let openModal23 = document.querySelector('.dog23');
+      let modal23 = document.querySelector('.modal23');
+      let closeModal23= document.querySelector('.modal_close23');
+      
+      openModal23.addEventListener('click', (e)=>{
+          e.preventDefault();
+          modal23.classList.add('modal--show23');
+      });
+      
+      closeModal23.addEventListener('click', (e)=>{
+          e.preventDefault();
+          modal23.classList.remove('modal--show23');
+      });
